e270638964fcd1d6a494649befcb5b78ef11170c
imjintaek <dana7923@naver.com>
2019-03-21 17:20:06 +0900

    추가test2

--- airplanproject/src/airplan/project/KoreanAir.java
+++ airplanproject/src/airplan/project/KoreanAir.java
@@ -5,6 +5,7 @@ public class KoreanAir {
 	public static void main(String[] args) {
 		System.out.println("민재야 완성!");
 		System.out.println("진택이형 안녕하세요");
+		System.out.println("6시 칼퇴 고고?");
 	}
 
 }
