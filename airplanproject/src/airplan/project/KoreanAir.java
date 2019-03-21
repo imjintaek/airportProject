1bd8e3a83d59b528734320837549d533f2b30715
Dfragment <38381030+Dfragment@users.noreply.github.com>
2019-03-21 17:38:17 +0900

    테스트2

--- airplanproject/src/airplan/project/KoreanAir.java
+++ airplanproject/src/airplan/project/KoreanAir.java
@@ -5,7 +5,8 @@ public class KoreanAir {
 	public static void main(String[] args) {
 		System.out.println("민재야 완성!");
 		System.out.println("진택이형 안녕하세요");
-		
+		System.out.println("6시 칼퇴 고고?");
+		System.out.println("바로가요");
 	}
 
 }
